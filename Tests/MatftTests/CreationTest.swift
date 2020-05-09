411b26103ce5c1cabb5579a97a9498619ceee004
jjjkkkjjj <kado_89_ssp_jun@yahoo.co.jp>
2020-05-09 20:56:43 +0900

    eye diag

---
+++ Tests/MatftTests/CreationTest.swift
@@ -0,0 +1,53 @@
+import XCTest
+//@testable import Matft
+import Matft
+
+final class CreationTests: XCTestCase {
+    
+    
+    func testDiag() {
+        do{
+            XCTAssertEqual(Matft.mfarray.diag(v: [3, -19, -22, 4]), MfArray([[  3,   0,   0,   0],
+                                                                             [  0, -19,   0,   0],
+                                                                             [  0,   0, -22,   0],
+                                                                             [  0,   0,   0,   4]]))
+            let a = MfArray([3, -19, -22, 4])
+            XCTAssertEqual(Matft.mfarray.diag(v: a), MfArray([[  3,   0,   0,   0],
+                                                              [  0, -19,   0,   0],
+                                                              [  0,   0, -22,   0],
+                                                              [  0,   0,   0,   4]]))
+        }
+
+        do{
+
+            XCTAssertEqual(Matft.mfarray.diag(v: [-0.87, 1.2, 5.5134, -8.78]), MfArray([[-0.87  ,  0.0    ,  0.0    ,  0.0    ],
+                                                                                        [ 0.0    ,  1.2   ,  0.0    ,  0.0    ],
+                                                                                        [ 0.0    ,  0.0    ,  5.5134,  0.0    ],
+                                                                                        [ 0.0    ,  0.0    ,  0.0    , -8.78  ]], mftype: .Double))
+            let a = MfArray([-0.87, 1.2, 5.5134, -8.78], mftype: .Double, mforder: .Column)
+            XCTAssertEqual(Matft.mfarray.diag(v: a), MfArray([[-0.87  ,  0.0    ,  0.0    ,  0.0    ],
+                                                              [ 0.0    ,  1.2   ,  0.0    ,  0.0    ],
+                                                              [ 0.0    ,  0.0    ,  5.5134,  0.0    ],
+                                                              [ 0.0    ,  0.0    ,  0.0    , -8.78  ]], mftype: .Double))
+
+        }
+        
+        
+    }
+    
+
+    func testEye(){
+        do{
+            XCTAssertEqual(Matft.mfarray.eye(dim: 3), MfArray([[ 1,  0,  0],
+                                                               [ 0,  1,  0],
+                                                               [ 0,  0,  1]]))
+        }
+        do{
+            XCTAssertEqual(Matft.mfarray.eye(dim: 3, mftype: .Float), MfArray([[ 1,  0,  0],
+                                                                               [ 0,  1,  0],
+                                                                               [ 0,  0,  1]], mftype: .Float))
+        }
+    }
+    
+
+}
