e0d9dd77d8a61bf1a0f06d366614b476fb46e0c4
jjjkkkjjj <kado_89_ssp_jun@yahoo.co.jp>
2020-05-11 13:48:33 +0900

    add method

--- Sources/Matft/core/method/conversion_mfarray.swift
+++ Sources/Matft/core/method/conversion_mfarray.swift
@@ -50,6 +50,71 @@ extension MfArray{
     public func broadcast_to(shape: [Int]) throws -> MfArray{
         return try Matft.mfarray.broadcast_to(self, shape: shape)
     }
+    /**
+       Create mfarray expanded dimension for given axis
+       - parameters:
+            - axis: the expanded axis
+    */
+    public static func expand_dims(axis: Int) -> MfArray{
+        return Matft.mfarray.expand_dims(self, axis: axis)
+    }
+    /**
+       Create mfarray expanded dimension for given axis
+       - parameters:
+            - axes: the list of expanded axes
+    */
+    public static func expand_dims(axes: [Int]) -> MfArray{
+        return Matft.mfarray.expand_dims(self, axes: axes)
+    }
+    /**
+       Create mfarray removed for 1-dimension
+       - parameters:
+            - axis: (Optional) the removed axis
+    */
+    public static func squeeze(axis: Int? = nil) -> MfArray{
+        return Matft.mfarray.squeeze(self, axis: axis)
+    }
+    /**
+       Create mfarray removed for 1-dimension
+       - parameters:
+            - mfarray: mfarray
+            - axes: the list of  removed axes
+    */
+    public static func squeeze(axes: [Int]) -> MfArray{
+        return Matft.mfarray.squeeze(self, axes: axes)
+    }
+    /**
+       Convert order of stored data.
+       - parameters:
+            - mforder: mforder
+    */
+    public static func conv_order(mforder: MfOrder) -> MfArray{
+        return Matft.mfarray.conv_order(self, mforder: mforder)
+    }
+    /**
+       Flatten 1d-mfarray
+       - parameters:
+            - mforder: (Optional) mforder, default is Row
+    */
+    public static func flatten(mforder: MfOrder = .Row) -> MfArray{
+        return Matft.mfarray.flatten(self, mforder: mforder)
+    }
+    /**
+       Reverse the mfarray order along given axis
+       - parameters:
+            - axis: (optional) the reversed axis
+    */
+    public static func flip(axis: Int? = nil) -> MfArray{
+        return Matft.mfarray.flip(self, axis: axis)
+    }
+    /**
+       Reverse the mfarray order along given axes
+       - parameters:
+            - axes: (optional) the reversed axis of list
+    */
+    public static func flip(_ mfarray: MfArray, axes: [Int]? = nil) -> MfArray{
+        return Matft.mfarray.flip(self, axes: axes)
+    }
 }
 /*
 extension MfData{
