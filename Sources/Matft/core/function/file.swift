8f4690d4126073f191d88d2d7166d75525fbede7
jjjkkkjjj <kado_89_ssp_jun@yahoo.co.jp>
2020-05-12 18:10:26 +0900

    genfromtxt

--- Sources/Matft/core/function/file.swift
+++ Sources/Matft/core/function/file.swift
@@ -25,7 +25,6 @@ extension Matft.mfarray.file{
             return _load(parser: parser, type: Double.self, mftype, skiprows, use_cols, max_rows)
         }
     }
-    
     public static func loadtxt(path: String, delimiter: Character, mftype: MfType = .Float, skiprows: [Int]? = nil, use_cols: [Int]? = nil, encoding: String.Encoding = .utf8, max_rows: Int? = nil, removeBlank: Bool = true) -> MfArray?{
         
         guard let parser = _TxtParser(path, delimiter, encoding, removeBlank) else { return nil }
@@ -39,23 +38,55 @@ extension Matft.mfarray.file{
         }
     }
     
-    public static func genfromtxt(){
-        /*
-        cannot load imbalance delimiter
-        e.g.) test.csv
-        1,2,0,
-        3,4,2
-        >>> np.genfromtxt('test.csv', delimiter=',')
-        ValueError: Some errors were detected !
-        Line #2 (got 3 columns instead of 4)
+    public static func genfromtxt<T: MfStorable>(url: URL, delimiter: Character, fillnan: T, mftype: MfType = .Float, skiprows: [Int]? = nil, use_cols: [Int]? = nil, encoding: String.Encoding = .utf8, max_rows: Int? = nil, removeBlank: Bool = true) -> MfArray?{
         
-        1,2,0,
-        3,4,2,
-        >>> np.genfromtxt('test.csv', delimiter=',')
-        array([[ 1.,  2.,  0., nan],
-               [ 3.,  4.,  2., nan]])
-        */
+        guard let parser = _TxtParser(url, delimiter, encoding, removeBlank) else { return nil }
+        
+        switch MfType.storedType(mftype) {
+        case .Float:
+            return _gen(parser: parser, fillnan: Float.from(fillnan), mftype, skiprows, use_cols, max_rows)
+            
+        case .Double:
+            return _gen(parser: parser, fillnan: Double.from(fillnan), mftype, skiprows, use_cols, max_rows)
+        }
     }
+    public static func genfromtxt(url: URL, delimiter: Character, mftype: MfType = .Float, skiprows: [Int]? = nil, use_cols: [Int]? = nil, encoding: String.Encoding = .utf8, max_rows: Int? = nil, removeBlank: Bool = true) -> MfArray?{
+        
+        guard let parser = _TxtParser(url, delimiter, encoding, removeBlank) else { return nil }
+        
+        switch MfType.storedType(mftype) {
+        case .Float:
+            return _gen(parser: parser, fillnan: Float.nan, mftype, skiprows, use_cols, max_rows)
+            
+        case .Double:
+            return _gen(parser: parser, fillnan: Double.nan, mftype, skiprows, use_cols, max_rows)
+        }
+    }
+    public static func genfromtxt<T: MfStorable>(path: String, delimiter: Character, fillnan: T, mftype: MfType = .Float, skiprows: [Int]? = nil, use_cols: [Int]? = nil, encoding: String.Encoding = .utf8, max_rows: Int? = nil, removeBlank: Bool = true) -> MfArray?{
+        
+        guard let parser = _TxtParser(path, delimiter, encoding, removeBlank) else { return nil }
+        
+        switch MfType.storedType(mftype) {
+        case .Float:
+            return _gen(parser: parser, fillnan: Float.from(fillnan), mftype, skiprows, use_cols, max_rows)
+            
+        case .Double:
+            return _gen(parser: parser, fillnan: Double.from(fillnan), mftype, skiprows, use_cols, max_rows)
+        }
+    }
+    public static func genfromtxt(path: String, delimiter: Character, mftype: MfType = .Float, skiprows: [Int]? = nil, use_cols: [Int]? = nil, encoding: String.Encoding = .utf8, max_rows: Int? = nil, removeBlank: Bool = true) -> MfArray?{
+        
+        guard let parser = _TxtParser(path, delimiter, encoding, removeBlank) else { return nil }
+        
+        switch MfType.storedType(mftype) {
+        case .Float:
+            return _gen(parser: parser, fillnan: Float.nan, mftype, skiprows, use_cols, max_rows)
+            
+        case .Double:
+            return _gen(parser: parser, fillnan: Double.nan, mftype, skiprows, use_cols, max_rows)
+        }
+    }
+
 }
 
 fileprivate func _load<T: MfStorable>(parser: _TxtParser, type: T.Type, _ mftype: MfType = .Float, _ skiprows: [Int]? = nil, _ use_cols: [Int]? = nil, _ max_rows: Int? = nil) -> MfArray?{
@@ -112,7 +143,71 @@ fileprivate func _load<T: MfStorable>(parser: _TxtParser, type: T.Type, _ mftype
     return MfArray(ret, mftype: mftype).squeeze()
 }
 
-
+fileprivate func _gen<T: MfStorable>(parser: _TxtParser, fillnan: T, _ mftype: MfType = .Float, _ skiprows: [Int]? = nil, _ use_cols: [Int]? = nil, _ max_rows: Int? = nil) -> MfArray?{
+    /*
+    cannot load imbalance delimiter
+    e.g.) test.csv
+    1,2,0,
+    3,4,2
+    >>> np.genfromtxt('test.csv', delimiter=',')
+    ValueError: Some errors were detected !
+    Line #2 (got 3 columns instead of 4)
+    
+    1,2,0,
+    3,4,2,
+    >>> np.genfromtxt('test.csv', delimiter=',')
+    array([[ 1.,  2.,  0., nan],
+           [ 3.,  4.,  2., nan]])
+    */
+    
+    guard let lines = parser.load() else {
+        return nil
+    }
+    
+    
+    var ret: [[T]] = []
+    var col_num = -1
+    for (row, line) in lines.enumerated(){
+        if skiprows?.contains(row) ?? false{
+            continue
+        }
+        if (max_rows ?? -1) == ret.count{
+            break
+        }
+        let arr_str = line.split(separator: parser.delimiter, omittingEmptySubsequences: false)
+        
+        // check column number
+        col_num = col_num == -1 ? arr_str.count : col_num
+        if arr_str.count != col_num{
+            print("Some errors were detected !\nLine #\(row) (got \(arr_str.count) columns instead of \(col_num)")
+            return nil
+        }
+        
+        var ret_line: [T] = []
+        for (col, str) in arr_str.enumerated(){
+            if !(use_cols?.contains(col) ?? true){
+                continue
+            }
+            
+            if str == ""{ // nan
+                ret_line += [fillnan]
+            }
+            else if let val = T.from(str){ // value
+                ret_line += [val]
+            }
+            else{
+                print("could not convert string to float: \"\(str)\"")
+                return nil
+            }
+            
+            
+        }
+        ret += [ret_line]
+    }
+    
+    //squeeze if one line only
+    return MfArray(ret, mftype: mftype).squeeze()
+}
 
 
 fileprivate class _TxtParser{
--- Tests/MatftTests/FileTest.swift
+++ Tests/MatftTests/FileTest.swift
@@ -61,5 +61,49 @@ final class FileTests: XCTestCase {
         }
         
     }
+    
+    func test_genfromtxt() {
+        do{
+            let test1 = self.get_url(filename: "test1.csv")
+            let arr1 = Matft.mfarray.file.genfromtxt(url: test1, delimiter: ",", mftype: .Float, encoding: .shiftJIS)
+            
+            XCTAssertNil(arr1)
+            
+            let test2 = self.get_url(filename: "test2.csv")
+            let arr2 = Matft.mfarray.file.genfromtxt(url: test2, delimiter: ",", mftype: .Float, encoding: .shiftJIS)
+            
+            XCTAssertEqual(arr2!, MfArray([[1, 2, 0],
+                                           [3, 4, 2]], mftype: .Float))
+            
+            let test3 = self.get_url(filename: "test3.csv")
+            let arr3 = Matft.mfarray.file.genfromtxt(url: test3, delimiter: ",", mftype: .Float, encoding: .shiftJIS)
+            let tmp3 = MfArray([[1, Float.nan, 0, Float.nan],
+                                [3, 4, 2,  Float.nan]], mftype: .Float)
+            // nan is handled as false.
+            XCTAssertEqual(arr3! === tmp3, MfArray([[true, false, true, false],
+                                                    [true, true, true, false]]))
+            
+            let test4 = self.get_url(filename: "test4.csv")
+            let arr4 = Matft.mfarray.file.genfromtxt(url: test4, delimiter: ",", mftype: .Float, encoding: .shiftJIS)
+            
+            XCTAssertNil(arr4)
+                
+        }
 
+        do{
+            let test4 = self.get_url(filename: "test4.csv")
+            let a1 = Matft.mfarray.file.genfromtxt(url: test4, delimiter: ",", mftype: .Float, skiprows: [0, 4], encoding: .shiftJIS)
+            
+            XCTAssertEqual(a1!, MfArray([[1, 0, 0, 6],
+                                         [3, 4, 2, 8],
+                                         [2, 3, 4, 1]], mftype: .Float))
+            
+            let a2 = Matft.mfarray.file.genfromtxt(url: test4, delimiter: ",", mftype: .Double, skiprows: [0, 4], encoding: .shiftJIS, max_rows: 2)
+            
+            XCTAssertEqual(a2!, MfArray([[1, 0, 0, 6],
+                                         [3, 4, 2, 8]], mftype: .Double))
+            
+        }
+        
+    }
 }
