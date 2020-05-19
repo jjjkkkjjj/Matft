fccf911b04c5241d522d7245d01228f9b549da51
jjjkkkjjj <kado_89_ssp_jun@yahoo.co.jp>
2020-05-20 08:27:37 +0900

    add in-place operator

---
+++ Sources/Matft/core/operator/in-place.swift
@@ -0,0 +1,24 @@
+//
+//  File.swift
+//  
+//
+//  Created by AM19A0 on 2020/05/20.
+//
+
+import Foundation
+
+public func +=(l_mfarray: inout MfArray, r_mfarray: MfArray){
+    l_mfarray = Matft.mfarray.add(l_mfarray, r_mfarray)
+}
+
+public func -=(l_mfarray: inout MfArray, r_mfarray: MfArray){
+    l_mfarray = Matft.mfarray.sub(l_mfarray, r_mfarray)
+}
+
+public func *=(l_mfarray: inout MfArray, r_mfarray: MfArray){
+    l_mfarray = Matft.mfarray.mul(l_mfarray, r_mfarray)
+}
+
+public func /=(l_mfarray: inout MfArray, r_mfarray: MfArray){
+    l_mfarray = Matft.mfarray.div(l_mfarray, r_mfarray)
+}
