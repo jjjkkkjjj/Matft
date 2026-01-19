bc7ab321346d53c90e38cac2ad225184bc5479ca
Pedro Gómez <pedro@goodnotesapp.com>
2026-01-19 13:37:27 +0100

    Provide now a mftype fallback types we when accelerate is not available

--- Sources/Matft/core/protocol/mfdataProtocol.swift
+++ Sources/Matft/core/protocol/mfdataProtocol.swift
@@ -6,12 +6,16 @@
 //
 
 import Foundation
+#if canImport(CoreML)
 import CoreML
+#endif
 
 public protocol MfDataBasable {}
 
 extension MfData: MfDataBasable{}
 
+#if canImport(CoreML)
 @available(macOS 10.13, *)
 @available(iOS 14.0, *)
 extension MLMultiArray: MfDataBasable{}
+#endif
